5a9416949fb65e4d3eeac0a140abd9fba58863f6
izzat <izzatfadzlon@gmail.com>
2024-02-15 23:07:40 +0800

    used HashRouter instead of BrowserRouter for client-side rendering

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { GlobalProvider } from "./context/GlobalContext";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalProvider>
       <ChakraProvider>
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </ChakraProvider>
     </GlobalProvider>
   </React.StrictMode>
