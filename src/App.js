acbe45712b3f1de8c2aeb013b91db5712764bfd9
izzat <izzatfadzlon@gmail.com>
2024-02-16 08:18:20 +0800

    Added Home index component. todo: Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Search from "./pages/Search";
 import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
           <Route path="search" element={<Search />} />
           <Route path="favorites" element={<Favorites />} />
         </Route>
