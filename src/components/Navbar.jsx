9be90c89d9161d48ce548fc1a9e5faeeb9705321
Izzat Fadzlon <52589461+kiblykat@users.noreply.github.com>
2024-05-03 10:44:09 +0800

    Added comments for Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
+/*
+  this file determines arrangement of items in the Navbar component at the top of the page
+*/
+
 import { SearchIcon } from "@chakra-ui/icons";
 import { StarIcon } from "@chakra-ui/icons";
 import {
@@ -14,11 +18,14 @@ import { NavLink } from "react-router-dom";
 import GlobalContext from "../context/GlobalContext";
 
 export default function Navbar() {
+  //uses context from GlobalContextProvider
   const globalCtx = useContext(GlobalContext);
+  //destructure parkList and getSlots from globalCtx
   const { parkList, getSlots } = globalCtx;
-
+  //create lastFetched state to update time displayed of lastFetched
   const [lastFetched, setLastFetched] = useState("");
 
+  //when parkList state changed, update the time of lastFetched to the update_datetime of index 0 in fetched data(format - update_datetime:"2024-05-03T10:21:08")
   useEffect(() => {
     let dateTime = parkList[0]?.update_datetime.slice(
       parkList[0].update_datetime.indexOf("T") + 1
@@ -28,6 +35,7 @@ export default function Navbar() {
 
   return (
     <>
+      {/* create a responsive wrapping Flex element, small screen: column, med screen:row + space btwn */}
       <Flex
         direction={{
           base: "column",
@@ -47,6 +55,7 @@ export default function Navbar() {
           <Heading>ParkNow</Heading>
         </Box>
 
+        {/* fix the stack direction to be horizontally aligned */}
         <HStack>
           <Button onClick={() => getSlots()}>Refresh</Button>
           <Text textAlign="center">Data Last Fetched: {lastFetched} </Text>
