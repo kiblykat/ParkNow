2443e25388825e7ff1ea64bec4cfac2196625a0a
Izzat Fadzlon <52589461+kiblykat@users.noreply.github.com>
2024-05-03 10:42:22 +0800

    Update mockapi.js
    added comment for mockapi.js

--- src/api/mockapi.js
+++ src/api/mockapi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//configure base URL of mock API for use in project
 const BASE_URL = "https://6597b34f668d248edf233795.mockapi.io/";
 const mockAPI = axios.create({ baseURL: BASE_URL });
 
