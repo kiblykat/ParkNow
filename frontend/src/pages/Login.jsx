bc601ae83eb9544eeb26c4e6f13fca6ed3823e13
izzat <izzatfadzlon@gmail.com>
2024-06-17 20:07:40 +0900

    finished basic loginPage no functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,28 +1,41 @@
-import { border } from "@chakra-ui/react";
+import { Box, Button, border } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { Input, FormControl, FormLabel } from "@chakra-ui/react";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div>
-        <label>Username: </label>
-        <input
-          style={{ border: "1px solid black", margin: "10px" }}
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          style={{ border: "1px solid black", margin: "10px" }}
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </div>
+    <div
+      style={{
+        marginTop: "50px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box maxW="700px" borderWidth="1px" borderRadius="lg" p="40px 150px">
+        <FormControl>
+          <FormLabel>Username: </FormLabel>
+          <Input
+            focusBorderColor="orange.700"
+            style={{ margin: "10px" }}
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password: </FormLabel>
+          <Input
+            focusBorderColor="orange.700"
+            style={{ margin: "10px" }}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </FormControl>
+        <Button>Log In</Button>
+      </Box>
     </div>
   );
 };
