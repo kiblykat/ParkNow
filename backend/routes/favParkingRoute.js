171a52ff7f9107f75dbd15c797868bafe0bbad24
izzat <izzatfadzlon@gmail.com>
2024-06-30 17:13:17 +0800

    backend: prevent duplicate favorites in db

--- backend/routes/favParkingRoute.js
+++ backend/routes/favParkingRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { favParkingModel } from "../model/favParkingModel.js";
 const router = express.Router();
 
+// READ ALL route
 router.get("/", async (req, res) => {
   try {
     const favParking = await favParkingModel.find({});
@@ -15,10 +16,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+// CREATE route
 router.post("/", async (req, res) => {
   try {
     if (!req.body.lotNo || !req.body.address || !req.body.availLots) {
       return res.status(400).send({ message: "send all required fields" });
+    }
+    const isAlrPresent = await favParkingModel.findOne({
+      address: req.body.address,
+    });
+    if (isAlrPresent) {
+      return res.status(400).send({ message: "favLot already present" });
     }
     const newFavLot = {
       lotNo: req.body.lotNo,
@@ -29,6 +37,17 @@ router.post("/", async (req, res) => {
     return res.status(201).send(newFavLot);
   } catch (err) {
     res.status(500).send({ error: err.message });
+  }
+});
+
+//DELETE route
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedLot = await favParkingModel.findByIdAndDelete(id);
+    return res.status(200).send(deletedLot);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
 });
 
