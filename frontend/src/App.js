b5c16c00c0f136725ed20e46983d7c7ca9bcf093
izzat <izzatfadzlon@gmail.com>
2024-08-21 22:24:27 +0800

    auth: create signUp page + register route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import RootLayout from "./layouts/RootLayout";
 import Search from "./pages/Search";
 import Favorites from "./pages/Favorites";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 // import Home from "./pages/Home.jsx.old";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/search" element={<Search />} />
           {/* <Route path="search" element={<Search />} /> */}
           <Route path="/favorites" element={<Favorites />} />
+          <Route path="/signup" element={<SignUp />} />
         </Route>
       </Routes>
     </HashRouter>
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,89 @@
+import React, { useContext, useState } from "react";
+import { Navigate, NavLink } from "react-router-dom";
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  Box,
+  Button,
+  Text,
+  HStack,
+} from "@chakra-ui/react";
+import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
+import AuthContext from "../context/AuthContext";
+
+const SignUp = () => {
+  const authCtx = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [isSigningIn, setIsSigningIn] = useState(false);
+
+  const { userLoggedIn } = authCtx;
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("logged in: " + userLoggedIn);
+    await doCreateUserWithEmailAndPassword(email, password);
+  };
+  return (
+    <div
+      style={{
+        marginTop: "50px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {console.log("logged in: " + userLoggedIn)}
+      {userLoggedIn && <Navigate to={"/search"} replace={true} />}
+      <Box
+        borderColor="orange.700"
+        maxW="700px"
+        borderWidth="1px"
+        borderRadius="lg"
+        p="40px 150px"
+      >
+        <Text as="b" fontSize="20px">
+          Sign Up
+        </Text>
+        <FormControl>
+          <FormLabel mt="30px">Username: </FormLabel>
+          <Input
+            borderColor="orange.200"
+            focusBorderColor="orange.700"
+            style={{ marginBottom: "10px" }}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password: </FormLabel>
+          <Input
+            borderColor="orange.200"
+            focusBorderColor="orange.700"
+            style={{ marginBottom: "10px" }}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </FormControl>
+        <HStack>
+          <Button
+            width="100%"
+            alignItems="center"
+            justifyContent="center"
+            onClick={onSubmit}
+          >
+            Sign Up
+          </Button>
+        </HStack>
+        <HStack>
+          <Text fontSize="sm">Already have an account?</Text>
+          <Text fontSize="sm" color="teal.500">
+            <NavLink to="/">Log In</NavLink>
+          </Text>
+        </HStack>
+      </Box>
+    </div>
+  );
+};
+
+export default SignUp;
