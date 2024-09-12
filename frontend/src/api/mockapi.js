374258e505a525f73a948176765cb17d56a8d54c
izzat <izzatfadzlon@gmail.com>
2024-09-12 20:56:00 +0800

    be:change backend to retrieve from hosted vercel

--- frontend/src/api/mockapi.js
+++ frontend/src/api/mockapi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 //configure base URL of mock API for use in project
 const BASE_URL = "https://park-now-api.vercel.app/";
+// const BASE_URL = "http://localhost:4000/";
 const parkingAPI = axios.create({ baseURL: BASE_URL });
 
 export default parkingAPI;
