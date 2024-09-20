63edbc38ce0420ad3b3b0ff63957f49a52e8702d
kiblykat <izzatfadzlon@gmail.com>
2024-09-20 10:47:25 +0800

    allow all origins

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://park-now-frontend.vercel.app',  // Frontend domain
+  origin: '*',  // Frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']  // Custom headers if needed
 }));
