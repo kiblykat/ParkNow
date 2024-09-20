e431929567fb1c115b12d98dfc704c6c08390dab
kiblykat <izzatfadzlon@gmail.com>
2024-09-20 10:42:24 +0800

    add authorization header into cors

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,11 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://park-now-frontend.vercel.app',  // Frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']  // Custom headers if needed
+}));
 app.use("/", favParkingRoute);
 
 const startServer = async () => {
